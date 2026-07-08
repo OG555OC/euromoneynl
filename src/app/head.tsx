5131338c676c67ae372fe16c2d8beecfadcf9b35
OG555OC <OG555OC@proton.me>
2026-07-08 22:26:18 +0300

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -11,7 +11,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '919643741158489');
+fbq('init', '2037749577132435');
 fbq('track', 'PageView');`,
         }}
       />
